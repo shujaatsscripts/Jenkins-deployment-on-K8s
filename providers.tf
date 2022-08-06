bb4f037941854ad89f0dfd7277c04cd1238012d2
Usama Shujaat <48332443+shujaatsscripts@users.noreply.github.com>
2022-08-06 16:16:16 +0500

    Terraform code

--- providers.tf
+++ providers.tf
@@ -1,13 +1,11 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.11.0"
-    }
-  }
-}
-
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "minikube"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+    config_context = "minikube"
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "request_storage" {
+  description = "storage for your jenkins installation"
+  default     = "20Gi"
+}
+
+variable "accessmode" {
+  description = "access mode for jenkins persistent volume claim"
+  default     = "ReadWriteOnce"
+}
+
+variable "name" {
+  description = "name of your jenkins application, will be used as prefix for all manifests"
+  default     = "jenkins"
+}
+
+variable "namespace" {
+  description = "namespace where all the jenkins resources will be created"
+  default     = "jenkins"
+}
+
+variable "storageclass" {
+  description = "Strogeclassname for PV"
+  default     = "jenkins-pv"
+}
+
+variable "serviceaccount" {
+  description = "service account for jenkins"
+  default     = "jenkins"
+}
+
+
+
