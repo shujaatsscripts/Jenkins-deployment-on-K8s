4bc625b6c99bd35085e556d4f2df14bc1e5f6f09
Usama Shujaat <48332443+shujaatsscripts@users.noreply.github.com>
2022-08-08 17:36:28 +0500

    update nginx paths

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo cp /etc/nginx/site-available/default /etc/nginx/site-available/default.backup
-sudo cp nginx-conf /etc/nginx/site-available/default
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.backup
+sudo cp ./nginx/nginx-conf /etc/nginx/sites-available/default
 sudo systemctl restart nginx
