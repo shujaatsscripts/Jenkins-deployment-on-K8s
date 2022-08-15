bab603a84389e9fcb9530a2c0bf760be8287696f
Usama Shujaat <48332443+shujaatsscripts@users.noreply.github.com>
2022-08-16 04:41:03 +0500

    updated tag for docker file

--- terraform/run.sh
+++ terraform/run.sh
@@ -3,6 +3,8 @@
 minikube start --memory 8192 --cpus 2
 minikube ssh 'sudo mkdir /data/jenkins-volume && sudo chown 1000:1000 -R /data/jenkins-volume'
 minikube ssh 'docker pull jenkins/jenkins:2.346.2-jdk11'
+sed -i "s|{{Password}}|Docker@1812|" terraform/jenkins-values.yaml
+sed -i "s|{{Username}}|devbreed|" terraform/jenkins-values.yaml
 #minikube ssh 'docker pull kiwigrid/k8s-sidecar:1.15.0'
 #minikube ssh 'docker pull jenkins/inbound-agent:4.11.2-4'
 #minikube ssh 'docker pull maorfr/kube-tasks:0.2.0'
